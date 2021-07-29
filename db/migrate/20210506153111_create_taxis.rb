9b5efdfd5e20bb53849a26749d7c914b1e329541
Nahum <losond8@gmail.com>
2021-07-28 18:37:28 -0700

    Create migrations, build migration manually, establish model relationships, updated seed list.

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.integer :cab_number
+      t.string :color
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Passenger.destroy_all
+Taxi.destroy_all
+Ride.destroy_all
+
+puts "Creating passengers ..."
+p1 = Passenger.create(name: 'Buggs Bunny', age: 6)
+p2 = Passenger.create(name: 'Daffy Duck', age: 5)
+p3 = Passenger.create(name: 'Tweet Bird', age: 8)
+
+puts "Creating taxis ..."
+c1 = Taxi.create(cab_number: 6564, color: 'Black')
+c2 = Taxi.create(cab_number: 8392, color: 'Yellow')
+
+puts "Creating rides ..."
+r1 = Ride.create(taxi_id: c1.id, passenger_id: p1.id)
+r2 = Ride.create(taxi_id: c2.id, passenger_id: p3.id)
